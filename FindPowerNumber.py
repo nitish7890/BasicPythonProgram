7bbd3eb02a3f4d896cce5f352b309f05d6e7e03f
Nitish <nitishmehta1997@gmail.com>
2021-11-10 09:24:58 +0530

    first commit

---
+++ FindPowerNumber.py
@@ -0,0 +1,7 @@
+def power(num):  #function definition
+  result=2**num;
+  print("Square of the number=>",result);
+
+#derived class
+num=int(input("Enter the value of Number"));
+power(num);   #calling function
---
+++ HarmonicNumber.py
@@ -0,0 +1,4 @@
+Num=int(input("Enter the value of range"));
+for i in range(1,Num+1):
+    Harmonic=Harmonic+1/i;
+print("Harmonic Number=>",Harmonic);
