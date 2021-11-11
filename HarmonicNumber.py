d221e3cc165ac8d27ab9f430395c3269a3124e86
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-11-11 09:27:21 +0530

    Update HarmonicNumber.py

--- HarmonicNumber.py
+++ HarmonicNumber.py
@@ -1,4 +1,12 @@
+def calc(Num):
+    try:
+        for i in range(1,Num+1):
+           Harmonic=0;
+           Harmonic=Harmonic+1/i;
+        print("Harmonic Number=>",Harmonic);  
+    except:
+        print("Value out of range");         
+
+#derived class
 Num=int(input("Enter the value of range"));
-for i in range(1,Num+1):
-    Harmonic=Harmonic+1/i;
-print("Harmonic Number=>",Harmonic);
+calc(Num);  
