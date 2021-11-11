7ef4765409f4587d753772f64d4c7bfc204f2029
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-11-11 09:25:14 +0530

    Update Factors.py

--- Factors.py
+++ Factors.py
@@ -1,10 +1,10 @@
 def checkPrimeOrNotPrime(num):
 # If given number is greater than 1
-
-    if num > 1:
+    try:
+        if num > 1:
  
     # Iterate from 2 to n / 2
-        for i in range(2, int(num/2)+1):
+          for i in range(2, int(num/2)+1):
  
         # If num is divisible by any number between
         # 2 and n / 2, it is not prime
@@ -14,9 +14,11 @@ def checkPrimeOrNotPrime(num):
             else:
         
                 print(num, "is a prime number")
-    else:       
+        else:       
     
-        print(num, "is not a prime number")
+            print(num, "is not a prime number")
+    except:
+        print("Please enter the correct Number");    
 
 #derived class
 num = int(input("Enter the value of Number"));
